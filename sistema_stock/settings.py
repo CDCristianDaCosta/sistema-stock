177b37fffdffb84ae89509560b31328e792b3259
CDCristianDaCosta <cristian.j.mcb@gmail.com>
2026-04-13 14:22:59 -0300

    fix cloudinary env

--- sistema_stock/settings.py
+++ sistema_stock/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import cloudinary
 
-cloudinary.config(
-    cloud_name="dchl3ztf0", api_key="Root", api_secret="VaV36jRicdXvXkpCo2o4kp5QMng"
-)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
