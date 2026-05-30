b36bed399380defc353a16b311284f6b5022f4b6
CDCristianDaCosta <cristian.j.mcb@gmail.com>
2026-05-30 10:27:09 -0300

    fix database settings

--- sistema_stock/settings.py
+++ sistema_stock/settings.py
@@ -92,17 +92,7 @@ WSGI_APPLICATION = "sistema_stock.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-
-if DATABASE_URL:
-    DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
