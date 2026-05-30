1fa3f2432a76ea330c738cafb30ca869f3fb4aac
CDCristianDaCosta <cristian.j.mcb@gmail.com>
2026-05-30 13:08:18 -0300

    fix database render

--- sistema_stock/settings.py
+++ sistema_stock/settings.py
@@ -92,12 +92,17 @@ WSGI_APPLICATION = "sistema_stock.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL =", DATABASE_URL)
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True
+    "default": dj_database_url.parse(
+        DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True,
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
